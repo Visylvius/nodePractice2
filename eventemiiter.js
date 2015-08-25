41407004c44dbe2e633a660e00bdff3a19545f8a
Loren Barrick <Lorenbarrick@gmail.com>
2015-08-24 21:59:39 -0700

    add pipe code

--- eventemiiter.js
+++ eventemiiter.js
@@ -26,3 +26,52 @@ logger.emit('error', 'Eggs Cracked');
   server.on('request', function(request, response){
 
   });
+
+  http.createServer(function(request, response){
+    response.writeHead(200);
+    var chunk = null;
+    while (null !== (chunk = request.read())) {
+      response.write(chunk);
+    }
+  });
+  request.on('end', function(){
+    response.end();
+  })
+}).listen(8080);
+// or you can do this
+http.createServer(function(request, response){
+  response.writeHead(200);
+  request.pipe(response);
+})
+
+var fs = require('fs');
+var file = fs.createReadStream('readme.md');
+var newFile = fs.createWriteSteam('readme_copy.md');
+file.pipe(newFile);
+
+var fs = require('fs');
+var http = require('http');
+
+http.createServer(function(request, response) {
+  var newFile = fs.createWriteSteam('readme_copy.md');
+  request.pipe(newFile);
+  request.on('end', function(){
+    response.end('uploaded!');
+  });
+}).listen(8080);
+// curl --upload-file read.me http://localhost:8080
+
+http.createServer(function(request, response) {
+  var newFile = fs.createWriteSteam('readme_copy.md');
+  var fileBytes = request.headers['content-length'];
+  var uploadedBytes = 0;
+  request.on('readable', function() {
+    var chunk = null;
+    while (null !== (chunk = request.read())) {
+      uploadedBytes += chunk.length;
+      var progress = (uploadedBytes / filesBytes) * 100;
+      response.write('progress:' + parseInt(progress, 10) + '%\n');
+    }
+  });
+  request.pipe(newFile);
+}).listen(8080);
