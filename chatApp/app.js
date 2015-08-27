29773234f529e144792049d6c76d4d27a40a286e
Loren Barrick <Lorenbarrick@gmail.com>
2015-08-26 19:24:30 -0700

    more routes

--- chatApp/app.js
+++ chatApp/app.js
@@ -8,7 +8,16 @@ io.on('connection', function(client){
   client.emit('messages', {hello: 'world'});
   client.on('messages', function(data) {
     console.log(data);
+    // get the nickname of the client before broadcasting message
+    var nickname = client.nickname;
+    //broadcast with the name and message
+    client.broadcast.emit('message', nickname + ": " + message);
+    //send the same message back to our client
+    client.emit('messages', nickname + ': ' + message);
     client.broadcast.emit('messages', data);
+  });
+  client.on('join', function(name) {
+    client.nickname = name;
   });
 });
 io.on('connection', function(client){
